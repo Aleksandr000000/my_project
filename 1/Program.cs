2534bc4da126aa86bea2954e66502ea218c44adb
Aleksandr <MrRotod@gmail.com>
2022-09-22 17:48:24 +0500

    калькулятор версия 1

---
+++ 1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("ваш рост: ");
+double   height = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ваш вес:  ");
+double   mass = Convert.ToInt32(Console.ReadLine());
+double   IMT = mass/((height*height)/10000);
+Console.Write($"индекс массы вашего тела составляет - {IMT} ");// необходимо округлить это число до сотых
