56f1fce2c61673406e8af585f72bcad1ac7f0e27
Aleksandr <MrRotod@gmail.com>
2022-09-25 19:11:03 +0500

    доработали калькулятор веса

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,5 +2,33 @@
 double   height = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ваш вес:  ");
 double   mass = Convert.ToInt32(Console.ReadLine());
+
+double ImtMass (double height, double mass) // метод вычисления ИМТ
+{
 double   IMT = Math.Round(mass/((height*height)/10000),2);
-Console.Write($"индекс массы вашего тела составляет - {IMT} ");
+return IMT;
+}
+
+
+string ConditionMass(double a) // метод определения состояния здоровья
+
+{
+    if (a < 16)
+        return ("дефецит веса");
+    if (a < 18.5 )
+        return ("недостаточная масса тела");
+    if (a < 24.99)
+        return("нормальная масса тела");
+    if (a < 30)
+        return ("избыватная масса тела");
+    if (a < 35 )
+        return ("ожирение");
+    if (a < 40 )
+        return ("резкое ожирение ");
+    else
+        return ("полный ппц");
+    
+}
+double a = ImtMass (height, mass);
+string b = ConditionMass(a);
+Console.WriteLine($"Индекс массы тела составляет {a} и это {b}");
